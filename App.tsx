05e0f0c0b108d93af3ee4e83585116f9ee2a9889
Felipe Werlang <felipewerlang.privado@gmail.com>
2026-02-07 22:31:30 -0300

    Create App.tsx as the main App component

---
+++ App.tsx
@@ -0,0 +1,12 @@
+// App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello from the App component!</h1>
+        </div>
+    );
+};
+
+export default App;
